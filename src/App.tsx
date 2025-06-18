a32b38ad8becae91ba59a40e9f3a16f278fecb52
Nathan Ryan <rockymountainshredder@gmail.com>
2025-06-18 01:21:54 -0600

    Deleted boilerplate css from App.css, edited App.tsx to remove initial test text

--- src/App.tsx
+++ src/App.tsx
@@ -8,11 +8,11 @@ function App() {
 
   return (
     <>
-      <div className="min-h-screen flex items-center justify-center bg-gradient-to-br from-blue-100 to-purple-200">
+      {/* <div className="min-h-screen flex items-center justify-center bg-gradient-to-br from-blue-100 to-purple-200">
         <h1 className="text-5xl font-bold text-green-700">
           Lightning ECE is working!
         </h1>
-      </div>
+      </div> */}
 
       <Routes>
         <Route path="/" element={<Layout />}>
