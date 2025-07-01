a519837075ad960490209abab7541d03726f97b3
Nathan Ryan <rockymountainshredder@gmail.com>
2025-07-01 13:08:06 -0600

    Wrapped App in HashRouter as opposed to BrowswerRouter for better compatibility with GitHub Pages for this stage of the project

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,20 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
-import { BrowserRouter } from 'react-router-dom';
+// import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import App from './App.tsx';
 import { AuthProvider } from "./context/AuthContext"; 
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter>
+    {/* <BrowserRouter> */}
+    <HashRouter>
+      {/* Using HashRouter for better compatibility with GitHub Pages */}
       <AuthProvider>
         <App />
       </AuthProvider>
-    </BrowserRouter>
+    </HashRouter>
+    {/* </BrowserRouter> */}
   </StrictMode>,
 )
